bcf40fe2953d5e9cac80b0f60b0678e2018f1145
Alexis Parave <aparavev@gmail.com>
2023-08-10 22:17:57 -0500

    :sparkles: Second testing commit

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello from Cloud Run 2')
+  res.send('Hello from Cloud Run')
 })
 
 app.get('/hello/:name', (req, res) => {
   res.send(`Hello ${req.params.name}`)
+})
+
+app.get('/success', (req, res) => {
+  res.send(`Deploy Success full`)
 })
 
 app.listen(process.env.PORT || 4000, () => {
